dc980be7cc885de5fed8223a8d3ec831d02e4e72
TanaraCavalcante <tanaracavalcante@icloud.com>
2025-01-19 17:46:21 +0100

    adjusted function 'greet'

--- secretsMessage.js
+++ secretsMessage.js
@@ -2,8 +2,9 @@
 
 // Can you help her?
 
-function greet(name){
-    return "Hello, " + name + "!";
-    if(name === "Johnny")
-      return "Hello, my love!";
+function greet(name){ 
+    if(name === "Johnny"){
+        return "Hello, my love!"
+    }
+      return "Hello, " + name + "!";
   }
