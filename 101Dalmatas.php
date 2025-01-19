95471be02f364b648ba48de984c928c1a7c2bf31
TanaraCavalcante <tanaracavalcante@icloud.com>
2025-01-19 18:11:43 +0100

    debug adjusted in 'howManyDogs' function

--- 101Dalmatas.php
+++ 101Dalmatas.php
@@ -8,10 +8,19 @@ The number of dogs will always be a number and there will always be at least 1 d
 
 <?php 
 
-function howManyDalmations($numer :
+function howManyDalmations($number){
+    $dogs = ["Hardly any", "More than a handful!", "Woah that's a lot of dogs!", "101 DALMATIANS!!!"];
+
+    if($number <= 10){
+        $respond = $dogs[0];
+    }elseif($number <= 50){
+        $respond = $dogs[1];
+    }else if($number === 101){
+        $respond = $dogs[3];
+    }else{
+        $respond = $dogs[2];
+    }
+
+    return $respond;
+}
   
-$dogs ["Hardly any", "More than a handful!", "Woah that's a lot of dogs!", "101 DALMATIONS!!!"];
-
-$respond = $number <= 10 ? $dogs[0] ($number <= 50 ? $dogs[1] : ($number = 101  $dogs[3] : $dogs[2]
-
-return $respond
